0eb9b576ffc52d138ee0a8499cc7fcf54f06f8c6
刘东 <liudong@ldmac.local>
2017-03-10 11:07:23 +0800

    框架搭建

---
+++ app/src/main/java/com/duma/liudong/meiye/base/BaseActivity.java
@@ -0,0 +1,68 @@
+package com.duma.liudong.meiye.base;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+
+import butterknife.ButterKnife;
+
+
+/**
+ * Created by Thinkpad on 2016/4/19.
+ */
+public abstract class BaseActivity extends AppCompatActivity {
+    public Activity mActivity;
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mActivity = this;
+        ActivityCollector.addActivity(this);
+        initContentView(savedInstanceState);
+        ButterKnife.bind(this);
+        initData();
+        loadData();
+        Other();
+    }
+
+    // 初始化UI，setContentView等
+    protected abstract void initContentView(Bundle savedInstanceState);
+
+    protected void initData() {
+    }
+
+    protected void loadData() {
+    }
+
+    protected void Other() {
+
+    }
+
+    protected void OnBack() {
+        finish();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK
+                && event.getAction() == KeyEvent.ACTION_DOWN) {
+            OnBack();
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+}
--- app/src/main/java/com/duma/liudong/meiye/base/MainActivity.java
+++ app/src/main/java/com/duma/liudong/meiye/base/MainActivity.java
@@ -1,7 +1,9 @@
-package com.duma.liudong.meiye;
+package com.duma.liudong.meiye.base;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.duma.liudong.meiye.R;
 
 public class MainActivity extends AppCompatActivity {
 
